4819ab01df12f0e726e87940d5bfb01ddde5fe6e
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-01-13 22:52:03 -0300

    updateapi

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://praticandojsonserver.netlify.app/'
+  baseURL: 'https://praticandojsonserver.netlify.app'
 })
 
 export default api
